0b0a6bf92bc1fc4d32e25753d79fa755aa31a580
YoungYa <1829594026@qq.com>
2026-01-19 17:05:41 +0800

    chore: 配置代理

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,14 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
+  server: {
+    host: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
