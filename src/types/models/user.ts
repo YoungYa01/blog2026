cb7e9ec674d99c2949b3bb0b8094ff12df827a4c
YoungYa <1829594026@qq.com>
2026-01-19 17:01:03 +0800

    feat: 增加user Model

---
+++ src/types/models/user.ts
@@ -0,0 +1,10 @@
+export type UserModel = {
+  age: number;
+  created_at: string;
+  email: string;
+  id: number;
+  is_active: boolean;
+  name: string;
+  updated_at: string;
+  uuid: string;
+};
