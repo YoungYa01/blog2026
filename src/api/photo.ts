4028ec41c5e843a17bdb75e1512a64192cf00d84
YoungYa <1829594026@qq.com>
2026-01-20 20:37:35 +0800

    fix: add photo api

---
+++ src/api/photo.ts
@@ -0,0 +1,18 @@
+import { http } from "@/api/request.ts";
+import { Photo } from "@/types/models/photo.ts";
+import {
+  PaginationModel,
+  PaginationResponse,
+  Response,
+} from "@/types/models/response.ts";
+
+export const getPhotoList = async (params: PaginationModel) =>
+  await http.get<PaginationResponse<Photo>>("/api/v1/photos", {
+    params,
+  });
+
+export const deletePhoto = async (id: string) =>
+  await http.delete<Response<string>>(`/api/v1/photos/${id}`);
+
+export const createPhoto = async (data: FormData) =>
+  await http.post<Response<Photo>>("/api/v1/photos/upload", data);
