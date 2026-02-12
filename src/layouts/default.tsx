000c2541f157de89778414eee54a17cf59959930
YoungYa <1829594026@qq.com>
2026-02-12 23:12:51 +0800

    fix: 调整无法垂直滚动问题

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -10,7 +10,7 @@ export default function DefaultLayout() {
 
   return (
     <BackgroundBeamsWithCollision className={"bg-black"}>
-      <div className="dark relative flex flex-col h-screen">
+      <div className="dark relative flex flex-col min-h-screen">
         <Navbar />
 
         <main
